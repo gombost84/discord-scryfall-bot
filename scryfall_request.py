3d684b32614e5160a7651b064c8fea5ba685e9e1
gombost84 <tamas.gombos84@gmail.com>
2021-11-21 12:11:42 +0100

    Now two sided cards are also handled

--- scryfall_request.py
+++ scryfall_request.py
@@ -7,11 +7,13 @@ def card_data(name):
     r = requests.get(endpoint + name)
 
     card_data_full = r.json()
-    card_data_needed = {
-        "image": card_data_full['image_uris']['large'],
-        "mana_cost": card_data_full['mana_cost'],
-        "type_line": card_data_full['type_line'],
-        "oracle_text": card_data_full['oracle_text']
-        }
+    card_data_needed = ""
 
-    return card_data_needed["image"]
+    if "image_uris" in card_data_full.keys():
+        card_data_needed = card_data_full['image_uris']['large']
+
+    else:
+        for element in card_data_full["card_faces"]:
+            card_data_needed  = card_data_needed + element['image_uris']['large'] + " "
+
+    return card_data_needed
